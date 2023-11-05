7d19da2a2c7eb438f8bcf14f3305252f4d2cc15d
piotrpdev <piotrpdev@gmail.com>
2023-11-05 21:04:13 +0000

    fix: package name in library bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-import { name } from './package.json';
 
 export default defineConfig({
     build: {
         lib: {
             entry: 'src/main/ts/Main.ts',
-            name: name.charAt(0).toUpperCase() + name.slice(1)
+            name: "Gitline"
         },
     },
     publicDir: 'src/main/public'
